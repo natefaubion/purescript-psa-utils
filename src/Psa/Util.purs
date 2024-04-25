03b91a8d1039ff5b26963f6554876b351399c993
Avinash Verma <avinashverma@Avinashs-MacBook-Pro.local>
2024-04-26 02:16:52 +0530

    Replace Monad with MonadRec, and used foldRecM in iter_

--- src/Psa/Util.purs
+++ src/Psa/Util.purs
@@ -1,9 +1,10 @@
 module Psa.Util where
 
 import Prelude
-import Data.Foldable (class Foldable, foldl)
+
+import Control.Monad.Rec.Class (class MonadRec)
+import Data.Array (foldRecM)
 import Data.String as Str
-import Data.Tuple (Tuple(..), snd)
 
 replicate :: forall m. (Monoid m) => Int -> m -> m
 replicate n m = go n mempty where
@@ -17,12 +18,7 @@ padLeft width str = replicate (width - Str.length str) " " <> str
 padRight :: Int -> String -> String
 padRight width str = str <> replicate (width - Str.length str) " "
 
-iter_ :: forall m f a b. Foldable f => Monad m => f a -> (Int -> a -> m b) -> m Unit
-iter_ xs f = snd $ foldl go (Tuple 0 (pure unit)) xs where
-  go (Tuple i a) b = 
-    Tuple (i + 1) 
-      (do
-        _ <- a
-        _ <- f i b
-        pure unit
-        )
+iter_ :: forall m a b. MonadRec m => Array a -> (Int -> a -> m b) -> m Unit
+iter_ xs f = void $ foldRecM go 0 xs where
+  go i a = (pure $ i + 1) <* f i a
+    
